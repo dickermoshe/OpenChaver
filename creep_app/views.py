08d4b7b76cb0e506db90c1d34fb92a4b975c5824
dickermoshe <dickermoshe@gmail.com>
2022-09-22 16:21:43 -0400

    ok

--- creep_app/views.py
+++ creep_app/views.py
@@ -1,4 +1,3 @@
-
 import logging
 from random import randint
 from creep_app.models import Screenshot
@@ -6,45 +5,62 @@ from creep_app.models import Screenshot
 from mss import mss
 from mss.windows import MSS
 from mss.exception import ScreenShotError
-from profanity_check import predict, predict_prob
+from profanity_check import predict
 import psutil
 
 from datetime import timedelta
 from django.utils import timezone
 
 
-from creep_app.brain import Brain
-from creep_app.models import Screenshot , Alert
+from creep_app.brain import NudeNet, splice_images, skin_pixels
+from creep_app.models import Screenshot, Alert
 
-logger = logging.getLogger('django')
+logger = logging.getLogger("service")
 
-def is_profane(s:str):
+
+def is_profane(s: str):
     return predict([s])[0] == 1
 
+
 def single_service(
-    request, min_queue:int = 10,max_queue: int = 20, cpu_threshold: int = 50, batch_size: int = 10, max_tries: int = 30, min_alert_time: int = 300,min_alert_size: int = 2
+    request,
+    min_queue: int = 2,
+    max_queue: int = 20,
+    stable_window: int = 5,
+    cpu_threshold: int = 50,
+    batch_size: int = 10,
+    max_tries: int = 30,
+    min_alert_time: int = 300,
+    min_alert_size: int = 2,
 ):
     """One Service that runs all the requsite services"""
 
     #: MSS client
     sct: MSS = mss()
+    logger.debug("MSS client created")
 
     #: Brain instance
-    brain = Brain()
+    brain = NudeNet()
+    logger.debug("Brain instance created")
 
     title = None
     raw_screenshots = []
 
     while True:
-        # Try to take a screenshot of the active window
         try:
-            logger.info("Taking screenshot")
-            image, title, exec_name = Screenshot.grab_screenshot(sct, max_tries=max_tries, invalid_title=title)
+            image, title, exec_name = Screenshot.grab_screenshot(
+                sct, max_tries=max_tries, invalid_title=title,stable_window = stable_window
+            )
             if image is None:
-                raise ScreenShotError("Could not take screenshot")
+                raise ScreenShotError()
+            
+            logger.debug("Screenshot taken")
+            logger.debug(f"Title: {title}")
+            logger.debug(f"Exec name: {exec_name}")
         except ScreenShotError:
-            logger.warning("MSS Error. Retrying...")
+            logger.exception("MSS Error. Recreating MSS client...")
             sct: MSS = mss()
+            logger.debug("MSS client re-created")
             continue
         except:
             logger.exception("Screenshot Error. Retrying...")
@@ -56,40 +72,67 @@ def single_service(
         # or
         # Title is profane
         # Then >> Save the screenshot
-        splices = brain.splice(image)
-        if (brain.skin_pixels(splices) or randint(1, 10) == 1 or is_profane(title)) and len(splices) > 0:
-            logger.info("Adding screenshot to queue")
-            splices = brain.match_size(splices)
-            raw_screenshots.append(dict(image=image, title=title, exec_name=exec_name,splices=splices))
+
+        # Splice the image
+        splices = splice_images(image)
+        logger.debug(f'Image spliced. {len(splices)} splices found')
+
+        # Check if the splices contains skin
+        skin = skin_pixels(splices) if len(splices) else False
+        logger.debug(f'Image contains skin: {skin}')
+
+        # Check if the title is profane
+        profane = is_profane(title)
+        logger.debug(f'Title is profane: {profane}')
+
+        # Random 1/10 chance if we should save the screenshot
+        random = randint(1, 10) == 1
+        logger.debug(f'Random Save Chance: {random}')
         
+        add_to_queue = skin or profane or random
+        logger.debug(f'Add to Queue: {add_to_queue}')
+
+        if add_to_queue:
+            raw_screenshots.append(
+                dict(image=image, title=title, exec_name=exec_name, splices=splices)
+            )
+
+        logger.debug(f'Queue Size: {len(raw_screenshots)}')
+
         # If the queue is empty, then continue
         if len(raw_screenshots) == 0:
+            logger.debug('Queue is empty. Continuing...')
             continue
 
         # Skip if the CPU is too high, unless the is a large queue
         if len(raw_screenshots) < max_queue and psutil.cpu_percent() > cpu_threshold:
             logger.info("CPU too high. Skipping...")
             continue
-            
+
         # If the queue is too small, then continue
         if len(raw_screenshots) < min_queue:
             logger.info("Queue too small. Skipping...")
             continue
-        
+
         logger.info("Processing queue")
         # Run detections on batches
         for raw_screenshot in raw_screenshots:
             logger.info("Running detections")
-            results = brain.detect(raw_screenshot['splices'])
+            if not len (raw_screenshot["splices"]):
+                continue
+            results = brain.detect(raw_screenshot["splices"],batch_size=batch_size)
             for result in results:
                 keep = randint(1, 20) == 1
-                if result['is_nsfw'] or keep:
-                    logger.info("NSFW detected")
-                    screenshot = Screenshot.save_screenshot(
-                        raw_screenshot['image'],
-                        raw_screenshot['title'],
-                        raw_screenshot['exec_name'],
-                        is_nsfw=True,
+                logger.debug(f'Keep: {keep}')
+                logger.debug(f'NSFW: {result["is_nsfw"]}')
+
+                if result["is_nsfw"] or keep:
+                    logger.info("Saving To Database")
+                    Screenshot.save_screenshot(
+                        raw_screenshot["image"],
+                        raw_screenshot["title"],
+                        raw_screenshot["exec_name"],
+                        is_nsfw=result["is_nsfw"],
                         keep=keep,
                     )
                     break
@@ -97,51 +140,38 @@ def single_service(
         # Clear the raw screenshots
         raw_screenshots = []
 
-        # Check if there are any alerts to send
-        logger.info("Checking for alerts")
-        nsfw_screenshots = Screenshot.objects.filter(alerts__isnull=True, is_nsfw=True)
-        if nsfw_screenshots.count() > 0:
-            groups = []
-            sub_group = [nsfw_screenshots[0]]
-            for s in nsfw_screenshots[1:]:
-                if (s.created - sub_group[-1].created).seconds < min_alert_time:
-                    sub_group.append(s)
-                else:
-                    groups.append(sub_group)
-                    sub_group = [s]
-            groups.append(sub_group)
-        
-            for group in groups:
-                if len(group) >= min_alert_size :
-                    alert = Alert.objects.create(screenshots = group)
-                    alert.send()
-        
-        # Delete old screenshots
-        screenshots = Screenshot.objects.filter(keep=False,is_nsfw=False)
-        for s in screenshots:
-            s.image.delete()
-            s.delete()
-        
-        # Delete all screenshots older than 3 days
-        screenshots = Screenshot.objects.filter(created__lt = timezone.now() - timedelta(days=3))
-        for s in screenshots:
-            s.image.delete()
-            s.delete()
+        # # Check if there are any alerts to send
+        # logger.info("Checking for alerts")
+        # nsfw_screenshots = Screenshot.objects.filter(alerts__isnull=True, is_nsfw=True)
+        # if nsfw_screenshots.count() > 0:
+        #     groups = []
+        #     sub_group = [nsfw_screenshots[0]]
+        #     for s in nsfw_screenshots[1:]:
+        #         if (s.created - sub_group[-1].created).seconds < min_alert_time:
+        #             sub_group.append(s)
+        #         else:
+        #             groups.append(sub_group)
+        #             sub_group = [s]
+        #     groups.append(sub_group)
 
-        # Delete all alerts older than 3 days
-        Alert.objects.filter(created__lt = timezone.now() - timedelta(days=3)).delete()
+        #     for group in groups:
+        #         if len(group) >= min_alert_size:
+        #             alert = Alert.objects.create(screenshots=group)
+        #             alert.send()
 
+        # # Delete old screenshots
+        # screenshots = Screenshot.objects.filter(keep=False, is_nsfw=False)
+        # for s in screenshots:
+        #     s.image.delete()
+        #     s.delete()
 
+        # # Delete all screenshots older than 3 days
+        # screenshots = Screenshot.objects.filter(
+        #     created__lt=timezone.now() - timedelta(days=3)
+        # )
+        # for s in screenshots:
+        #     s.image.delete()
+        #     s.delete()
 
-
-
-
-                    
-        
-        
-
-
-
-            
-
-
+        # # Delete all alerts older than 3 days
+        # Alert.objects.filter(created__lt=timezone.now() - timedelta(days=3)).delete()
--- creep_app/window.py
+++ creep_app/window.py
@@ -1,31 +1,37 @@
 import logging
 import win32ui
 import win32process as wproc
+import ctypes
+import cv2 as cv
 import psutil
 import numpy as np
 
 logger = logging.getLogger(__name__)
+
+user32 = ctypes.windll.user32
 
 class Window:
     def __init__(self, hwnd):
         self.hwnd = hwnd
         
         try:
-            logger.debug("Getting window title")
             self.title = self.hwnd.GetWindowText()
         except:
-            logger.exception("Unable to get window title")
             self.title = "Unknown Title"
         
         try:
-            logger.debug("Getting window PID")
             self.pid = wproc.GetWindowThreadProcessId(self.hwnd.GetSafeHwnd())[1]
-            logger.debug("Getting process name")
             self.exec_name = psutil.Process(self.pid).name()
         except:
             logger.exception("Unable to get window pid / exec_name")
             self.pid = -1
             self.exec_name = "Unknown Executable"
+        
+        try:
+            self.dpi = user32.GetDpiForWindow(self.hwnd.GetSafeHwnd())
+            print(self.dpi)
+        except:
+            self.dpi = 96
         
     def __str__(self):
         return self.title
@@ -36,18 +42,15 @@ class Window:
     def get_coordinates(self):
         
         try:
-            logger.debug("Getting raw client coordinates...")
             client_rect = self.hwnd.GetClientRect()
             logger.debug(f"Client rect: {client_rect}")
             
-            logger.debug("Getting raw window coordinates...")
             window_rect = self.hwnd.GetWindowRect()
             logger.debug(f'Window rect: {window_rect}')
         except:
             logger.exception("Unable to get window coordinates")
             return None
 
-        logger.debug("Calculating coordinates...")
         client_width = client_rect[2] - client_rect[0]
         window_width = window_rect[2] - window_rect[0]
         border = (window_width - client_width) // 2
@@ -65,6 +68,12 @@ class Window:
         coordinates = self.get_coordinates()
         image = sct.grab(coordinates)
         image = np.array(image)[:, :, :3]
+
+        # Scale image to 96 DPI
+        if self.dpi != 96:
+            scale = self.dpi / 96
+            image = cv.resize(image, None, fx=scale, fy=scale)
+
         return image
     
     def is_stable(self):
