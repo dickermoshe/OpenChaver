3dad07ffe67c969bf1c0aa934a1c943115d13ec9
dickermoshe <dickermoshe@gmail.com>
2022-10-14 00:44:21 -0400

    up version

--- install.nsi
+++ install.nsi
@@ -1,5 +1,5 @@
 # set the name of the installer
-!define VERSION "0.1.0"
+!define VERSION "0.1.1"
 Name "OpenChaver ${VERSION}"
 Outfile "Installer.exe"
  
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="openchaver_client",
-    version='0.1.0',
+    version='0.1.1',
     author="Moshe Dicker",
     author_email='mail@openchaver.com',
     description="An open source alternative to WebChaver.",
